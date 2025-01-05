071d5af915cb791bf1c55099195b0854f33dbb3c
Nikita_Vel <velikihnikita301@gmail.com>
2025-01-05 13:08:28 +0200

    init

---
+++ bin/dart_fizz_buzz.dart
@@ -0,0 +1,5 @@
+import 'package:dart_fizz_buzz/dart_fizz_buzz.dart' as dart_fizz_buzz;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_fizz_buzz.calculate()}!');
+}
---
+++ test/dart_fizz_buzz_test.dart
@@ -0,0 +1,8 @@
+import 'package:dart_fizz_buzz/dart_fizz_buzz.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
